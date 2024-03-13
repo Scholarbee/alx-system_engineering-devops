0578a89f55cb9f107646399fd4839587d7927ed4
Scholarbee <99612272+Scholarbee@users.noreply.github.com>
2024-03-13 01:17:29 +0000

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,21 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
+'''package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}'''
+
+class flask_installation {
+
+  # Package resource to install pip3
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  # Exec resource to install Flask version 2.1.0 using pip3
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/usr/local/bin'],
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  }
 }
